c20548d6e8cec0a4c127d09b230eb6ad7de8ca40
Andrieli Javorski <andrieli.javorski@grupo-3c.com>
2025-10-15 09:39:16 -0300

    challenge 21 accomplished

--- challenge-21/challenge-21.js
+++ challenge-21/challenge-21.js
@@ -1,17 +1,46 @@
-/*
-O desafio de hoje será um pequeno projeto: um cronômetro!
-As regras para criação do cronômetro são as seguintes:
-1. Crie um arquivo index.html e adicione esse script a ele;
-2. Crie um campo `input` do tipo `text`, e inicie-o com um valor 0 (zero).
-Ele será o nosso cronômetro;
-3. Crie 3 botões para as ações do cronômetro: Start, Stop e Reset;
-4. Ao clicar em Start, o valor do campo deve ser incrementado de 1 em 1, a
-cada segundo;
-5. Ao clicar em Stop, o cronômetro deve parar de contar;
-6. Ao clicar em Reset, o cronômetro deve zerar e parar de contar.
+(function(doc){
+    /*
+    O desafio de hoje será um pequeno projeto: um cronômetro!
+    As regras para criação do cronômetro são as seguintes:
+    1. Crie um arquivo index.html e adicione esse script a ele;
+    2. Crie um campo `input` do tipo `text`, e inicie-o com um valor 0 (zero).
+    Ele será o nosso cronômetro;
+    3. Crie 3 botões para as ações do cronômetro: Start, Stop e Reset;
+    4. Ao clicar em Start, o valor do campo deve ser incrementado de 1 em 1, a
+    cada segundo;
+    5. Ao clicar em Stop, o cronômetro deve parar de contar;
+    6. Ao clicar em Reset, o cronômetro deve zerar e parar de contar.
+    
+    Utilize o atributo data-js para nomear o campo e os botões. Você pode
+    usar o nome que achar melhor, desde que ele seja semântico, ou seja, o nome
+    dado ao elemento HTML deve definir o que o elemento é ou o que ele faz.
+    */
+    // ?
 
-Utilize o atributo data-js para nomear o campo e os botões. Você pode
-usar o nome que achar melhor, desde que ele seja semântico, ou seja, o nome
-dado ao elemento HTML deve definir o que o elemento é ou o que ele faz.
-*/
-// ?
+    var $inputText = doc.querySelector( 'input[data-js="cronometro"]' )
+    var $buttonStart = doc.querySelector( 'button[data-js="btn-start"]' )
+    var $buttonStop = doc.querySelector( 'button[data-js="btn-stop"]' )
+    var $buttonReset = doc.querySelector( 'button[data-js="btn-reset"]' )
+
+    var counter;
+
+    function start(){
+        $inputText.value++;
+        counter = setTimeout(() => {
+            start();
+        }, 1000);
+    }
+
+    function stop(){
+        clearTimeout(counter);
+    }
+
+    function reset(){
+        stop();
+        $inputText.value = 0;
+    }
+
+    $buttonStart.addEventListener( 'click', start, false );
+    $buttonStop.addEventListener( 'click', stop, false );
+    $buttonReset.addEventListener( 'click', reset, false );
+})(document);
