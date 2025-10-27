ed4fc84debc53d906409fd6d33f278318630745b
Andrieli Javorski <andrieli.javorski@grupo-3c.com>
2025-10-27 11:55:31 -0300

    challenge 28 accomplished

--- challenge-28/challenge-28.js
+++ challenge-28/challenge-28.js
@@ -1,3 +1,5 @@
+(function(win, doc){
+  'use strict';
   /*
   No HTML:
   - Crie um formulário com um input de texto que receberá um CEP e um botão
@@ -25,3 +27,92 @@
   - Utilize a lib DOM criada anteriormente para facilitar a manipulação e
   adicionar as informações em tela.
   */
+
+  function DOM(elements) {
+    this.elements = doc.querySelectorAll(elements);
+  }
+
+  DOM.prototype.on = function on( event, action ) {
+    Array.prototype.forEach.call( this.elements, item => item.addEventListener( event, action))
+  }
+
+  // DOM.prototype.off = function off( event, action ) {
+  //   this.elements.array.forEach(item => {
+  //     item.removeEventListner( event, action, false )
+  //   });
+  // }
+
+  DOM.prototype.get = function get() {
+    return this.elements;
+  }
+
+  // function objType (obj){
+  //   return Object.prototype.toString.call(obj)
+  // }
+
+  // DOM.prototype.forEach = function (){ return Array.prototype.forEach.apply(this.elements, arguments) };
+  // DOM.prototype.map = function (){ return Array.prototype.map.apply(this.elements, arguments) };
+  // DOM.prototype.filter = function (){ return Array.prototype.filter.apply(this.elements, arguments) };
+  // DOM.prototype.reduce = function (){ return Array.prototype.reduce.apply(this.elements, arguments) };
+  // DOM.prototype.reduceRight = function (){ return Array.prototype.reduceRight.apply(this.elements, arguments) };
+  // DOM.prototype.every = function (){ return Array.prototype.every.apply(this.elements, arguments) };
+  // DOM.prototype.some = function (){ return Array.prototype.some.apply(this.elements, arguments) };
+
+  // DOM.prototype.isArray = function (obj){ return objType(obj) === '[object Array]'};
+  // DOM.prototype.isObject = function (obj){ return objType(obj) === '[object Object]'};
+  // DOM.prototype.isFunction = function (obj){ return objType(obj) === '[object Function]'};
+  // DOM.prototype.isNumber = function (obj){ return objType(obj) === '[object Number]'};
+  // DOM.prototype.isString = function (obj){ return objType(obj) === '[object String]'};
+  // DOM.prototype.isBoolean = function (obj){ return objType(obj) === '[object Boolean]'};
+  // DOM.prototype.isNull = function (obj){ return objType(obj) === '[object Null]' || objType(obj) === '[object Undefined]'};
+
+  const ajax = new XMLHttpRequest;
+  const $formZipCode = new DOM('[data-js="form-zip-code"]');
+  const $zipCode = new DOM('[data-js="input-zip-code"]');
+  const $requestStatus = new DOM('[data-js="input-status"]');
+  const $street = new DOM('[data-js="input-street"]');
+  const $neighborhood = new DOM('[data-js="input-neighborhood"]');
+  const $state = new DOM('[data-js="input-state"]');
+  const $city = new DOM('[data-js="input-city"]');
+  const $zipCodeResponse = new DOM('[data-js="input-zip-code-response"]');
+
+  $formZipCode.on( 'submit', handleSubmitForm );
+
+  function handleSubmitForm(e){
+    e.preventDefault();
+    const cleanZip = $zipCode.get()[0].value.match(/\d/g).join('');
+    showMessage('loading');
+    ajaxRequest( 'GET', `https://viacep.com.br/ws/${cleanZip}/json/` );
+  };
+
+  function ajaxRequest( method, url){
+    ajax.open( method, url );
+    ajax.send();
+    ajax.addEventListener( 'readystatechange', handleReadyStateChange );
+  }
+
+  function handleReadyStateChange(){
+    if( ajax.readyState === 4 && ajax.status === 200) return fillFields();
+    if( ajax.status !== 200 ) showMessage('error');
+  }
+
+  function fillFields(){
+    const { logradouro, bairro, uf, localidade, cep } = JSON.parse(ajax.responseText);
+    $street.get()[0].value = logradouro;
+    $neighborhood.get()[0].value = bairro;
+    $state.get()[0].value = uf;
+    $city.get()[0].value = localidade;
+    $zipCodeResponse.get()[0].value = cep;
+    showMessage('sucess');
+  }
+
+  function showMessage(status) {
+    const statusMessage = {
+      'loading': `Buscando informações para o CEP ${$zipCode.get()[0].value}...`,
+      'sucess': `Endereço referente ao CEP ${$zipCode.get()[0].value}:`,
+      'error': `Não encontramos o endereço para o CEP ${$zipCode.get()[0].value}.`
+    };
+    return $requestStatus.get()[0].textNode = statusMessage[status];
+  }
+
+})(window, document);
